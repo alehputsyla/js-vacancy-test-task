e93a5b1705e330b3821ca96e5dfa37f1532a549f
Aleh Putsyla <alehputsyla@gmail.com>
2024-01-22 16:14:04 +0300

    Add payment-failed page
    Add payment-failed page and update routes

---
+++ apps/web/src/pages/payment-failed/index.page.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import { NextPage } from 'next';
+import { Button, Center, Image, Paper, rem, Stack, Text, Title } from '@mantine/core';
+
+import { Link } from 'components';
+import { RoutePath } from 'routes';
+
+const PaymentFailed: NextPage = () => (
+  <>
+    <Head>
+      <title>Payment Failed</title>
+    </Head>
+
+    <Center mt={rem(84)}>
+      <Paper w={rem(480)} radius="lg">
+        <Stack p="lg" gap="xl" align="center">
+          <Image
+            alt="Payment Failed"
+            src="/images/payment-failed-icon.webp"
+            w={rem(56)}
+          />
+          <Stack gap="md" align="center">
+            <Title order={1} size="h2">Payment Failed</Title>
+            <Text c="dimmed" ta="center">
+              Sorry, your payment failed.
+              <br />
+              Would you like to try again?
+            </Text>
+          </Stack>
+          <Link type="router" href={RoutePath.MyCart} underline={false}>
+            <Button w={rem(186)}>Back to Cart</Button>
+          </Link>
+        </Stack>
+      </Paper>
+    </Center>
+  </>
+);
+
+export default PaymentFailed;
