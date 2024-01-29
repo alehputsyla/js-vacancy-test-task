7e748679c76a710bd6b1955c743d2cede6834d35
Aleh Putsyla <alehputsyla@gmail.com>
2024-01-29 10:16:48 +0300

    Update product create action

--- apps/api/src/resources/product/actions/create.ts
+++ apps/api/src/resources/product/actions/create.ts
@@ -1,26 +1,48 @@
+import { Next } from 'koa';
+import multer from '@koa/multer';
+
 import { z } from 'zod';
 
-import { AppKoaContext, AppRouter, ProductUseCreateInput, ProductUseCreateOutput } from 'types';
+import { AppKoaContext, AppRouter } from 'types';
+
+import { cloudStorageService } from 'services';
 
 import { productService } from 'resources/product';
 
 import { validateMiddleware } from 'middlewares';
 
+const upload = multer();
+
+const ONE_MB_IN_BYTES = 1048576;
+const IMAGE_FORMATS = ['image/png', 'image/jpg', 'image/jpeg'];
+
+async function validator(ctx: AppKoaContext, next: Next) {
+  const { file } = ctx.request;
+
+  ctx.assertClientError(file, { photo: 'File cannot be empty' });
+  ctx.assertClientError(file.size / ONE_MB_IN_BYTES < 2, { photo: 'Sorry, you cannot upload a photo larger than 2 MB.' });
+  ctx.assertClientError(IMAGE_FORMATS.includes(file.mimetype), { photo: 'Sorry, you can only upload JPG, JPEG or PNG photos.' });
+
+  await next();
+}
+
 const schema = z.object({
-  price: z.number().min(1, 'Please enter Price'),
+  price: z.coerce.number().min(1, 'Please enter Price'),
   title: z.string().min(1, 'Please enter Title').max(100),
-  quantity: z.number().min(1, 'Please enter Quantity'),
-  photoUrl: z.string().min(1, 'Please upload Product\'s photo'),
+  quantity: z.coerce.number().min(1, 'Please enter Quantity'),
 });
 
-type ValidationSchema = z.infer<typeof schema>;
-type ValidatedData = {
-  [K in keyof ValidationSchema]: K extends keyof ProductUseCreateInput ? ProductUseCreateInput[K] : never;
-};
+type ValidatedData = z.infer<typeof schema>;
 
-async function handler(ctx: AppKoaContext<ValidatedData> & { body: ProductUseCreateOutput }) {
-  const { price, title, quantity, photoUrl } = ctx.validatedData;
+async function handler(ctx: AppKoaContext<ValidatedData>) {
+  const { price, title, quantity } = ctx.validatedData;
   const { user } = ctx.state;
+  const { file } = ctx.request;
+
+  const fileName = `${user._id}-${Date.now()}-${file.originalname}`;
+  const { Location: photoUrl } = await cloudStorageService.uploadPublic(`product/${fileName}`, file);
+
+  ctx.assertClientError(photoUrl, { photo: 'An error occurred while uploading a photo. Try again.' });
 
   await productService.insertOne({
     price,
@@ -34,5 +56,5 @@ async function handler(ctx: AppKoaContext<ValidatedData> & { body: ProductUseCre
 }
 
 export default (router: AppRouter) => {
-  router.post('/create', validateMiddleware(schema), handler);
+  router.post('/create', upload.single('file'), validator, validateMiddleware(schema), handler);
 };
