d4217c5461fb3a769a84f5c8e43ce22dd861102b
Aleh Putsyla <alehputsyla@gmail.com>
2024-01-29 10:14:11 +0300

    Update product types

--- packages/types/src/product.types.ts
+++ packages/types/src/product.types.ts
@@ -3,9 +3,3 @@ import { z } from 'zod';
 import { productSchema } from 'schemas';
 
 export type Product = z.infer<typeof productSchema>;
-
-export type ProductUseCreateInput = Pick<Product, 'title' | 'price' | 'quantity' | 'photoUrl'>;
-export type ProductUseCreateOutput = {};
-
-export type ProductUseUploadPhotoInput = FormData;
-export type ProductUseUploadPhotoOutput = Pick<Product, 'photoUrl'>;
