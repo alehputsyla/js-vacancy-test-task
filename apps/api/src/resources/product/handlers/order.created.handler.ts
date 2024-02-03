493077e070a7a70a1a878ef7290d458ccaecc902
Aleh Putsyla <alehputsyla@gmail.com>
2024-02-03 23:55:54 +0300

    Create handlers for update product's quantity when order create or expires

---
+++ apps/api/src/resources/product/handlers/order.created.handler.ts
@@ -0,0 +1,29 @@
+import { eventBus, InMemoryEvent } from '@paralect/node-mongo';
+
+import { Order } from 'types';
+
+import { DATABASE_DOCUMENTS } from 'app-constants';
+
+import logger from 'logger';
+
+import { productService } from 'resources/product';
+
+const { ORDERS } = DATABASE_DOCUMENTS;
+
+eventBus.on(`${ORDERS}.created`, async (data: InMemoryEvent<Order>) => {
+  try {
+    const { products } = data.doc;
+
+    const updatePromises = products.map( async ({ _id, quantity: cartQuantity }) => {
+      await productService.updateOne(
+        { _id },
+        ({ quantity: productQuantity }) => ({ quantity: productQuantity - cartQuantity }), 
+      );
+    });
+
+    await Promise.all(updatePromises);
+
+  } catch (err) {
+    logger.error(`${ORDERS}.created handler error: ${err}`);
+  }
+});
