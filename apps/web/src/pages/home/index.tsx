14c28c23618af8bad2a276496c809ea540b88d6e
Aleh Putsyla <alehputsyla@gmail.com>
2024-02-05 11:25:35 +0300

    Add skeleton and fix all state of marketplace

--- apps/web/src/pages/home/index.tsx
+++ apps/web/src/pages/home/index.tsx
@@ -119,63 +119,204 @@ const Home: NextPage = () => {
 
   const { addToCart, isCartContain } = useCart();
 
-  let products: JSX.Element;
+  const products = data && data.marketplaceCount > 0 && data.items.length > 0
+    && (
+    <SimpleGrid cols={{ base: 1, sm: 2, md: 3, lg: 2, xl: 3 }}>
+      {data.items.map((product) => (
+        <Card key={product._id} padding="md" radius="lg" withBorder>
+          <Card.Section>
+            <Image
+              src={product.photoUrl}
+              height={218}
+              alt={product.title}
+            />
+          </Card.Section>
 
-  if (data?.marketplaceCount === 0) {
-    products = (
-      <Center>
-        <Container p={75}>
-          <Text size="xl" c="gray">
-            Marketplace is empty!
-          </Text>
-        </Container>
-      </Center>
+          <Text fz="xl" fw={700} mt="md" truncate="end">{product.title}</Text>
+
+          <Group justify="space-between" align="center" mt="md">
+            <Text fz="md" c="dimmed">Price:</Text>
+            <Text fz="xl" fw={700}>
+              <NumberFormatter prefix="$" thousandSeparator value={product.price} />
+            </Text>
+          </Group>
+
+          <Button
+            color="blue"
+            fullWidth
+            mt="lg"
+            radius="md"
+            onClick={() => addToCart(product)}
+            disabled={isCartContain(product._id)}
+          >
+            {isCartContain(product._id) ? 'In cart' : 'Add to cart'}
+          </Button>
+        </Card>
+      ))}
+    </SimpleGrid>
     );
-  } else if (data?.items.length) {
-    products = (
-      <SimpleGrid cols={{ base: 1, sm: 2, md: 3, lg: 2, xl: 3 }}>
-        {data.items.map((product) => (
-          <Card key={product._id} padding="md" radius="lg" withBorder>
-            <Card.Section>
-              <Image
-                src={product.photoUrl}
-                height={218}
-                alt={product.title}
-              />
-            </Card.Section>
 
-            <Text fz="xl" fw={700} mt="md" truncate="end">{product.title}</Text>
+  const emptyMarketplace = data?.marketplaceCount === 0
+      && (
+      <Container p={75}>
+        <Text size="xl" c="gray">
+          Marketplace is empty!
+        </Text>
+      </Container>
+      );
 
-            <Group justify="space-between" align="center" mt="md">
-              <Text fz="md" c="dimmed">Price:</Text>
-              <Text fz="xl" fw={700}>
-                <NumberFormatter prefix="$" thousandSeparator value={product.price} />
-              </Text>
-            </Group>
-
-            <Button
-              color="blue"
-              fullWidth
-              mt="lg"
-              radius="md"
-              onClick={() => addToCart(product)}
-              disabled={isCartContain(product._id)}
-            >
-              {isCartContain(product._id) ? 'In cart' : 'Add to cart'}
-            </Button>
-          </Card>
-        ))}
-      </SimpleGrid>
-    );
-  } else {
-    products = (
+  const emptySearch = data && data.marketplaceCount > 0 && data.items.length === 0
+      && (
       <Container p={75}>
         <Text size="xl" c="gray">
           No results found, try to adjust your search or filters.
         </Text>
       </Container>
-    );
-  }
+      );
+
+  const filters = (
+    <Skeleton radius="lg" visible={isListLoading}>
+      <Paper p="lg" radius="lg" withBorder>
+        <Stack gap="lg">
+          <Group justify="space-between">
+            <Text fz="xl" fw={700}>Filters</Text>
+            <Button variant="shell" p={0} onClick={() => handleResetAllFilters()} disabled={data?.marketplaceCount === 0}>
+              <Text fz="sm" pr={rem(4)}>Reset All</Text>
+              <IconX size={16} />
+            </Button>
+          </Group>
+          <Stack gap="xs">
+            <Text fz="md" fw={700}>Price</Text>
+            <SimpleGrid cols={2}>
+              <NumberInput
+                leftSectionPointerEvents="none"
+                leftSection={<Text fw={500} fz="sm">From: </Text>}
+                leftSectionWidth={rem(54)}
+                suffix="$"
+                allowNegative={false}
+                hideControls
+                fw={500}
+                fz="sm"
+                radius="md"
+                thousandSeparator=" "
+                value={`${filterPrice.min}`}
+                onChange={(value) => handleFilterPrice({ max: filterPrice.max, min: +value })}
+                error={filterPriceError}
+                disabled={data?.marketplaceCount === 0}
+              />
+              <NumberInput
+                leftSectionPointerEvents="none"
+                leftSection={<Text fw={500} fz="sm">To: </Text>}
+                leftSectionWidth={rem(36)}
+                suffix="$"
+                allowNegative={false}
+                hideControls
+                fw={500}
+                radius="md"
+                thousandSeparator=" "
+                value={`${filterPrice.max}`}
+                onChange={(value) => handleFilterPrice({ min: filterPrice.min, max: +value })}
+                error={filterPriceError}
+                disabled={data?.marketplaceCount === 0}
+              />
+            </SimpleGrid>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Skeleton>
+  );
+
+  const searchInput = (
+    <Skeleton radius="md" visible={isListLoading}>
+      <TextInput
+        leftSection={<IconSearch size={16} />}
+        leftSectionPointerEvents="none"
+        placeholder="Type to search..."
+        radius="md"
+        value={search}
+        onChange={setSearch}
+        rightSection={search ? (
+          <UnstyledButton
+            display="flex"
+            onClick={() => setSearch('')}
+          >
+            <IconX color="gray" />
+          </UnstyledButton>
+        ) : null}
+        disabled={data?.marketplaceCount === 0}
+      />
+    </Skeleton>
+  );
+
+  const contentHeader = (
+    <Stack>
+      <Group justify="space-between">
+        <Skeleton visible={isListLoading} w="fit-content">
+          <Text fw={700}>{`${data?.count || 0} results`}</Text>
+        </Skeleton>
+
+        <Skeleton visible={isListLoading} w="fit-content">
+          <Button
+            variant="shell"
+            display="flex"
+            style={{ alignItems: 'center' }}
+            c="dimmed"
+            onClick={() => handleSort(sortBy === 'newest' ? 'oldest' : 'newest')}
+            disabled={data?.marketplaceCount === 0}
+          >
+            <IconArrowsDownUp style={{ width: rem(20), height: rem(20) }} />
+            <Text c="black" fw={500} mx={rem(4)}>{`Sort by ${sortBy}`}</Text>
+            <IconChevronDown style={{ width: rem(16), height: rem(16) }} />
+          </Button>
+        </Skeleton>
+      </Group>
+
+      <Group>
+        {filterPrice.min && filterPrice.max && !filterPriceError && (
+        <Paper component={Group} pl={rem(20)} py={rem(10)} pr={rem(17)} radius="xl" withBorder fz="sm" gap={rem(8)} lh={rem(1)}>
+          <Group gap={0}>
+            <NumberFormatter value={filterPrice.min} prefix="$" />
+            -
+            <NumberFormatter value={filterPrice.max} prefix="$" />
+          </Group>
+          <CloseButton bg="dark.3" c="white" radius="xl" size="xs" onClick={() => handleResetAllFilters()} />
+        </Paper>
+        )}
+      </Group>
+    </Stack>
+  );
+
+  const loadingState = isListLoading
+      && (
+      <SimpleGrid cols={{ base: 1, sm: 2, md: 3, lg: 2, xl: 3 }}>
+        {[1, 2, 3, 4, 5, 6].map((key) => (
+          <Card key={key} padding="md" radius="lg" withBorder>
+            <Card.Section>
+              <Skeleton height={218} />
+            </Card.Section>
+
+            <Skeleton height={rem(27)} radius="md" mt="md" />
+
+            <Group justify="space-between" align="center" mt="md">
+              <Skeleton height={rem(24)} width={rem(64)} radius="md" mt="md" />
+              <Skeleton height={rem(24)} width={rem(64)} radius="md" mt="md" />
+            </Group>
+
+            <Skeleton height={rem(40)} radius="md" mt="md" />
+          </Card>
+        ))}
+      </SimpleGrid>
+      );
+
+  const pagination = data && data.count > PER_PAGE && (
+    <Center>
+      <Pagination
+        value={params.page}
+        total={data.totalPages}
+        onChange={(targetPage) => setParams((prev) => ({ ...prev, page: targetPage }))}
+      />
+    </Center>
+  );
 
   return (
     <>
@@ -185,148 +326,29 @@ const Home: NextPage = () => {
 
       <Grid gutter="xl">
         <Grid.Col span={{ base: 12, lg: 4, xl: 3 }}>
-          <Paper p="lg" radius="lg" withBorder>
-            <Stack gap="lg">
-              <Group justify="space-between">
-                <Text fz="xl" fw={700}>Filters</Text>
-                <Button variant="shell" p={0} onClick={() => handleResetAllFilters()} disabled={data?.marketplaceCount === 0}>
-                  <Text fz="sm" pr={rem(4)}>Reset All</Text>
-                  <IconX size={16} />
-                </Button>
-              </Group>
-              <Stack gap="xs">
-                <Text fz="md" fw={700}>Price</Text>
-                <SimpleGrid cols={2}>
-                  <NumberInput
-                    leftSectionPointerEvents="none"
-                    leftSection={<Text fw={500} fz="sm">From: </Text>}
-                    leftSectionWidth={rem(54)}
-                    suffix="$"
-                    allowNegative={false}
-                    hideControls
-                    fw={500}
-                    fz="sm"
-                    radius="md"
-                    thousandSeparator=" "
-                    value={`${filterPrice.min}`}
-                    onChange={(value) => handleFilterPrice({ max: filterPrice.max, min: +value })}
-                    error={filterPriceError}
-                    disabled={data?.marketplaceCount === 0}
-                  />
-                  <NumberInput
-                    leftSectionPointerEvents="none"
-                    leftSection={<Text fw={500} fz="sm">To: </Text>}
-                    leftSectionWidth={rem(36)}
-                    suffix="$"
-                    allowNegative={false}
-                    hideControls
-                    fw={500}
-                    radius="md"
-                    thousandSeparator=" "
-                    value={`${filterPrice.max}`}
-                    onChange={(value) => handleFilterPrice({ min: filterPrice.min, max: +value })}
-                    error={filterPriceError}
-                    disabled={data?.marketplaceCount === 0}
-                  />
-                </SimpleGrid>
-              </Stack>
-            </Stack>
-          </Paper>
+          {filters}
         </Grid.Col>
 
         <Grid.Col span={{ base: 12, lg: 8, xl: 9 }}>
           <Stack gap="lg">
-            <TextInput
-              leftSection={<IconSearch size={16} />}
-              leftSectionPointerEvents="none"
-              placeholder="Type to search..."
-              radius="md"
-              value={search}
-              onChange={setSearch}
-              rightSection={search ? (
-                <UnstyledButton
-                  display="flex"
-                  onClick={() => setSearch('')}
-                >
-                  <IconX color="gray" />
-                </UnstyledButton>
-              ) : null}
-              disabled={data?.marketplaceCount === 0}
-            />
+            {searchInput}
 
-            <Stack>
-              <Group justify="space-between">
-                {data && <Text fw={700}>{`${data.count} results`}</Text>}
-
-                <Button
-                  variant="shell"
-                  display="flex"
-                  style={{ alignItems: 'center' }}
-                  c="dimmed"
-                  onClick={() => handleSort(sortBy === 'newest' ? 'oldest' : 'newest')}
-                  disabled={data?.marketplaceCount === 0}
-                >
-                  <IconArrowsDownUp style={{ width: rem(20), height: rem(20) }} />
-                  <Text c="black" fw={500} mx={rem(4)}>{`Sort by ${sortBy}`}</Text>
-                  <IconChevronDown style={{ width: rem(16), height: rem(16) }} />
-                </Button>
-              </Group>
-
-              <Group>
-                {filterPrice.min && filterPrice.max && !filterPriceError && (
-                <Paper component={Group} pl={rem(20)} py={rem(10)} pr={rem(17)} radius="xl" withBorder fz="sm" gap={rem(8)} lh={rem(1)}>
-                  <Group gap={0}>
-                    <NumberFormatter value={filterPrice.min} prefix="$" />
-                    -
-                    <NumberFormatter value={filterPrice.max} prefix="$" />
-                  </Group>
-                  <CloseButton bg="dark.3" c="white" radius="xl" size="xs" onClick={() => handleResetAllFilters()} />
-                </Paper>
-                )}
-              </Group>
-            </Stack>
+            {contentHeader}
 
             {products}
 
-            {isListLoading
-                && (
-                <SimpleGrid cols={{ base: 1, sm: 2, md: 3, lg: 2, xl: 3 }}>
-                  {[1, 2, 3, 4, 5, 6].map((key) => (
-                    <Card key={key} padding="md" radius="lg" withBorder>
-                      <Card.Section>
-                        <Skeleton height={218} />
-                      </Card.Section>
+            {emptyMarketplace}
 
-                      <Skeleton height={rem(27)} radius="md" mt="md" />
+            {emptySearch}
 
-                      <Group justify="space-between" align="center" mt="md">
-                        <Skeleton height={rem(24)} width={rem(64)} radius="md" mt="md" />
-                        <Skeleton height={rem(24)} width={rem(64)} radius="md" mt="md" />
-                      </Group>
-
-                      <Skeleton height={rem(40)} radius="md" mt="md" />
-                    </Card>
-                  ))}
-                </SimpleGrid>
-                )}
-
+            {loadingState}
           </Stack>
         </Grid.Col>
 
         <Grid.Col span={12}>
-          <Center>
-            {data && data.count > PER_PAGE && (
-            <Pagination
-              value={params.page}
-              total={data.totalPages}
-              onChange={(targetPage) => setParams((prev) => ({ ...prev, page: targetPage }))}
-            />
-            )}
-          </Center>
+          {pagination}
         </Grid.Col>
       </Grid>
-
-      <Group grow preventGrowOverflow={false} gap="xl" align="flex-start" />
     </>
   );
 };
